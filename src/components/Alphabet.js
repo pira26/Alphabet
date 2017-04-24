a39d5a8c7827ef9e677b1d931ad841d7a22cb390
Pira <sritharan.pirathaab@gmail.com>
2017-04-24 21:35:09 +0200

    Alphabet.js

--- src/components/Alphabet.js
+++ src/components/Alphabet.js
@@ -11,11 +11,17 @@ export default class Alphabet extends Component {
 		this.state = {
 			alphabets,
 			currentPosition:0,
-			currentTick:0
+			currentTick:0, 
+			random: false,
+			sound: true
 		};
+
 		this.prev = this.prev.bind(this);
 		this.next = this.next.bind(this);
 		this.playSound = this.playSound.bind(this);
+		this.manualPlaySound = this.manualPlaySound.bind(this);
+		this.switchRandom = this.switchRandom.bind(this);
+		this.switchSound = this.switchSound.bind(this);
 	}
 
 	componentDidMount() {
@@ -38,15 +44,68 @@ export default class Alphabet extends Component {
 		let letterSound = document.querySelector(`audio[data-key="letter sound"]`);
 		let wordSound = document.querySelector(`audio[data-key="word sound"]`);
 		
-		this.state.currentTick === 0 ? letterSound.currentTime = 0 & letterSound.play() : wordSound.currentTime = 0 & wordSound.play();
-		// if (this.state.currentTick === 0) {
+		this.state.sound ? (this.state.currentTick === 0 ? letterSound.currentTime = 0 & letterSound.play() : wordSound.currentTime = 0 & wordSound.play()) : "";
+		
+		// if(this.state.sound) {
+		// 	if (this.state.currentTick === 0) {
 		// 	letterSound.currentTime = 0;
 		// 	letterSound.play();
+		// 	}
+		// 	else { 
+		// 		wordSound.currentTime = 0; 
+		// 		wordSound.play();
+		// 	}
 		// }
-		// else { 
-		// 	wordSound.currentTime = 0; 
-		// 	wordSound.play();
-		// }
+	}
+
+
+	manualPlaySound() {
+		let letterSound = document.querySelector(`audio[data-key="letter sound"]`);
+		let wordSound = document.querySelector(`audio[data-key="word sound"]`);
+		
+		this.state.currentTick === 0 ? letterSound.currentTime = 0 & letterSound.play() : wordSound.currentTime = 0 & wordSound.play();
+
+	}
+
+	randomNumber(min, max) {
+		return Math.floor(Math.random() * (max-min + 1) + min);
+	}
+
+	next() {
+		console.log('next button clicked');
+		
+	// 	this.state.random ? 
+	// 		(this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick+1}) : this.setState({currentPosition: , currentTick: 0})) : 
+	// 	this.state.currentPosition === this.state.alphabets.length-1 ? 
+	// 		(this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick+1}) : this.setState({currentPosition:0, currentTick:0})) :
+	// 	this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick+1}) : this.setState({currentPosition: this.state.currentPosition+1, currentTick: 0});
+	// 	
+		if(this.state.random) {
+			if(this.state.currentTick < 2) {
+				this.setState({currentTick: this.state.currentTick+1});
+			}
+			else {
+				this.setState({currentPosition: this.randomNumber(0,25), currentTick: 0});
+			}
+		}
+		else {
+			if(this.state.currentPosition === this.state.alphabets.length-1) {
+				if(this.state.currentTick < 2) {
+					this.setState({currentTick: this.state.currentTick+1});
+				}
+				else {
+					this.setState({currentPosition:0, currentTick:0});
+				}
+			}
+			else {
+				if(this.state.currentTick < 2) {
+					this.setState({currentTick: this.state.currentTick+1});
+				}
+				else {
+					this.setState({currentPosition: this.state.currentPosition+1, currentTick: 0});
+				}
+			}
+		}
 	}
 
 	prev() {
@@ -54,11 +113,16 @@ export default class Alphabet extends Component {
 		this.state.currentPosition > 0 ? this.setState({currentPosition: this.state.currentPosition-1}) : this.setState({currentPosition: this.state.alphabets.length-1});
 	}
 
-	next() {
-		console.log('next button clicked');
-		this.state.currentPosition === this.state.alphabets.length-1 ? (this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick+1}) : this.setState({currentPosition:0, currentTick:0})) :
-		this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick+1}) : this.setState({currentPosition: this.state.currentPosition+1, currentTick: 0});
+
+	switchRandom() {
+		this.setState({random: !this.state.random});
+		this.next();
 	}
+
+	switchSound() {
+		this.setState({sound: !this.state.sound});
+	}
+
 
 	render() {
 		let showImg = this.state.currentTick !== 0 ? true : false;
@@ -66,6 +130,24 @@ export default class Alphabet extends Component {
 		console.log(showImg)  
 		return(
 			<div className="game">
+				<span className="random-label">Random letters: </span>
+				<label className="switch">
+					<input type="checkbox"
+						   defaultValue="false"
+						   checked={this.state.random}
+						   onClick={this.switchRandom} />
+					<div className="slider round"></div>
+				</label>
+
+				<span className="random-label">Sound: </span>
+				<label className="switch">
+					<input type="checkbox"
+						   defaultValue="false"
+						   checked={this.state.sound}
+						   onClick={this.switchSound} />
+					<div className="slider round"></div>
+				</label>
+
 				<div className="option">
 					<div className="fields">
 						<div className="field-block">{this.state.alphabets[this.state.currentPosition].letter}</div>
@@ -78,7 +160,7 @@ export default class Alphabet extends Component {
 						<a className="button prev"
 						   onClick={this.prev}>Previous</a>
 						<a className="button sound"
-						   onClick={this.playSound}>Play Sound Again</a>
+						   onClick={this.manualPlaySound}>Play Sound</a>
 						<a className="button next"
 						   onClick={this.next}>Next</a>
 					</div>
