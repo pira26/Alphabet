bbce6b7ab7093d562473fe53e5a60049c0a27fc5
Pira <sritharan.pirathaab@gmail.com>
2017-04-24 20:32:29 +0200

    Alphabet.js

--- src/components/Alphabet.js
+++ src/components/Alphabet.js
@@ -14,24 +14,50 @@ export default class Alphabet extends Component {
 			currentTick:0
 		};
 		this.prev = this.prev.bind(this);
-		this.again = this.prev.bind(this);
 		this.next = this.next.bind(this);
+		this.playSound = this.playSound.bind(this);
+	}
+
+	componentDidMount() {
+		let letterSound = document.querySelector(`audio[data-key="letter sound"]`);
+		let wordSound = document.querySelector(`audio[data-key="word sound"]`);
+		console.log(letterSound, wordSound);
+
+		this.state.currentPosition === 0 ? letterSound.currentTime = 0 & letterSound.play() : "";
+		// if(this.state.currentPosition === 0) {
+		// 	letterSound.currentTime = 0;
+		// 	letterSound.play();
+		// }
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		this.playSound();
+	}
+
+	playSound() {
+		let letterSound = document.querySelector(`audio[data-key="letter sound"]`);
+		let wordSound = document.querySelector(`audio[data-key="word sound"]`);
+		
+		this.state.currentTick === 0 ? letterSound.currentTime = 0 & letterSound.play() : wordSound.currentTime = 0 & wordSound.play();
+		// if (this.state.currentTick === 0) {
+		// 	letterSound.currentTime = 0;
+		// 	letterSound.play();
+		// }
+		// else { 
+		// 	wordSound.currentTime = 0; 
+		// 	wordSound.play();
+		// }
 	}
 
 	prev() {
 		console.log('prev button clicked');
-		this.setState({
-			currentPosition: this.state.currentPosition -1
-		});
-	}
-
-	again() {
-		console.log('again button clicked');
+		this.state.currentPosition > 0 ? this.setState({currentPosition: this.state.currentPosition-1}) : this.setState({currentPosition: this.state.alphabets.length-1});
 	}
 
 	next() {
 		console.log('next button clicked');
-		this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick +1}) : this.setState({currentPosition: this.state.currentPosition +1, currentTick: 0});
+		this.state.currentPosition === this.state.alphabets.length-1 ? (this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick+1}) : this.setState({currentPosition:0, currentTick:0})) :
+		this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick+1}) : this.setState({currentPosition: this.state.currentPosition+1, currentTick: 0});
 	}
 
 	render() {
@@ -43,12 +69,16 @@ export default class Alphabet extends Component {
 				<div className="option">
 					<div className="fields">
 						<div className="field-block">{this.state.alphabets[this.state.currentPosition].letter}</div>
+						<audio src={this.state.alphabets[this.state.currentPosition].letterSound}
+							   data-key="letter sound" />
 					</div>
+					position: {this.state.currentPosition} <br/>
+					tick: {this.state.currentTick}
 					<div className="buttons">
 						<a className="button prev"
 						   onClick={this.prev}>Previous</a>
 						<a className="button sound"
-						   onClick={this.again}>Play Sound Again</a>
+						   onClick={this.playSound}>Play Sound Again</a>
 						<a className="button next"
 						   onClick={this.next}>Next</a>
 					</div>
@@ -59,6 +89,8 @@ export default class Alphabet extends Component {
 								<img src={this.state.alphabets[this.state.currentPosition].image} 
 									 alt="picture corresponding to letter"
 									 className={classNames("letter-image", {hide: !showImg})}/>
+								<audio src={this.state.alphabets[this.state.currentPosition].wordSound}
+							   		   data-key="word sound" />
 							</div>
 							<div className="rigth-field">
 								<div className={classNames("placeholder-span", {hide: showWord})}>Click next to see the text</div>
