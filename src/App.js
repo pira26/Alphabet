71d034f4ac179b9a30ae7dffaad6b0b1368abc8a
Pira <sritharan.pirathaab@gmail.com>
2017-04-24 18:54:20 +0200

    App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Alphabet from './components/Alphabet';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Alphabet />
+      </div>
+    );
+  }
+}
+
+export default App;
