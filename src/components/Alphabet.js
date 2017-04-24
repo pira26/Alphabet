98108462b82e94512fc446b9777f7cedb6a904f9
Pira <sritharan.pirathaab@gmail.com>
2017-04-24 19:32:02 +0200

    Alphabet.js

--- src/components/Alphabet.js
+++ src/components/Alphabet.js
@@ -1,4 +1,5 @@
 import  React, {Component} from 'react';
+import classNames from 'classnames';
 
 import alphabets from './alphabets.json';
 import './alphabet.css';
@@ -9,35 +10,59 @@ export default class Alphabet extends Component {
 		super(props);
 		this.state = {
 			alphabets,
-			currentPosition:0
+			currentPosition:0,
+			currentTick:0
 		};
+		this.prev = this.prev.bind(this);
+		this.again = this.prev.bind(this);
+		this.next = this.next.bind(this);
+	}
+
+	prev() {
+		console.log('prev button clicked');
+		this.setState({
+			currentPosition: this.state.currentPosition -1
+		});
+	}
+
+	again() {
+		console.log('again button clicked');
+	}
+
+	next() {
+		console.log('next button clicked');
+		this.state.currentTick < 2 ? this.setState({currentTick: this.state.currentTick +1}) : this.setState({currentPosition: this.state.currentPosition +1, currentTick: 0});
 	}
 
 	render() {
+		let showImg = this.state.currentTick !== 0 ? true : false;
+		let showWord = this.state.currentTick === 2 ? true : false;
+		console.log(showImg)  
 		return(
 			<div className="game">
 				<div className="option">
 					<div className="fields">
-						<div className="field-block">
-							{this.state.alphabets[this.state.currentPosition].letter}
-						</div>
+						<div className="field-block">{this.state.alphabets[this.state.currentPosition].letter}</div>
 					</div>
 					<div className="buttons">
-						<a className="button prev">Previous</a>
-						<a className="button sound">Play Sound Again</a>
-						<a className="button next">Next</a>
+						<a className="button prev"
+						   onClick={this.prev}>Previous</a>
+						<a className="button sound"
+						   onClick={this.again}>Play Sound Again</a>
+						<a className="button next"
+						   onClick={this.next}>Next</a>
 					</div>
 					<div className="fields">
 						<div className="field-block">
 							<div className="left-field">
-								<div className="placeholder-span hide">Click next to see the image</div>
+								<div className={classNames("placeholder-span", {hide: showImg})}>Click next to see the image</div>
 								<img src={this.state.alphabets[this.state.currentPosition].image} 
 									 alt="picture corresponding to letter"
-									 className="letter-image"/>
+									 className={classNames("letter-image", {hide: !showImg})}/>
 							</div>
 							<div className="rigth-field">
-								<div className="placeholder-span hide">Click next to see the text</div>
-								<div className="word">{this.state.alphabets[this.state.currentPosition].word.toUpperCase()}</div>
+								<div className={classNames("placeholder-span", {hide: showWord})}>Click next to see the text</div>
+								<div className={classNames("word", {hide: !showWord})}>{this.state.alphabets[this.state.currentPosition].word.toUpperCase()}</div>
 							</div>
 						</div>
 					</div>
